a3c6777a87c138743b000d2c4abec5d536d7d2bb
Pedro Fachetti Carvalhom <pfachetti.pc@gmail.com>
2023-02-06 03:54:41 -0500

    please work

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,6 @@ import logging
 
 from flask import Flask
 
-"""
- Logging configuration
-"""
-
-logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
-logging.getLogger().setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 app.config.from_object("config")
